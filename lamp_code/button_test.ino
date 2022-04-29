a5ed45efa33d2f7e786e81639a82639ed3e87c1f
martincangaa <91851271+martincangaa@users.noreply.github.com>
2022-04-29 14:59:36 +0200

    Update button_test.ino

--- lamp_code/button_test.ino
+++ lamp_code/button_test.ino
@@ -1,19 +1,19 @@
 // 1.0 Version (mapping lamp modes) (Problem: Too slow!!)
 
-const int button = 2;
+const int BUTTON = 2;
 int button_touches = 0;     // this variable = lamp mode
 
 void setup(){
-    pinMode(button, INPUT);
+    pinMode(BUTTON, INPUT);
     Serial.begin(9600);
 }
 
 void loop(){
-    if (digitalRead(button) == HIGH and button_touches < 2)
+    if (digitalRead(BUTTON) == HIGH and button_touches < 2)
     {
         delay(100);
 
-        if (digitalRead(button) == LOW)
+        if (digitalRead(BUTTON) == LOW)
         {
             button_touches++;
             Serial.println(button_touches);
@@ -21,9 +21,9 @@ void loop(){
         }
         
     }
-    if (digitalRead(button) == HIGH and button_touches == 2)
+    if (digitalRead(BUTTON) == HIGH and button_touches == 2)
     {   
-        if (digitalRead(button) == LOW)
+        if (digitalRead(BUTTON) == LOW)
         {
         button_touches =0;
         Serial.println(button_touches);
@@ -34,28 +34,46 @@ void loop(){
 }
 
 
-// 1.1 Version (mapping lamp modes) (Problem: If you hold the button, arduino registers multiple interactions)
+// 1.1 Version (mapping lamp modes) (Problem: If you hold the BUTTON, arduino registers multiple interactions)
 
 int button_touches = 0;
 
 void setup(){
-    pinMode(button, INPUT);
+    pinMode(BUTTON, INPUT);
     Serial.begin(9600);
 }
 
 void loop(){
-    if (digitalRead(button) == HIGH and button_touches < 2)
+    if (digitalRead(BUTTON) == HIGH and button_touches < 2)
     {
     	button_touches++;
         Serial.println(button_touches);
       	delay(250);
     }
         
-    if (digitalRead(button) == HIGH and button_touches == 2)
+    if (digitalRead(BUTTON) == HIGH and button_touches == 2)
     {
         button_touches =0;
         Serial.println(button_touches);
       	delay(250);
     }
     
+}
+
+
+// 1.2 Version (mapping lamp modes) (I'm using OneButton library: https://github.com/mathertel/OneButton)
+
+#include "OneButton.h"
+#define BUTTON 2
+int button_touches = 0;
+
+OneButton button(BUTTON, false);
+
+void setup(){
+    
+    button.attachLongPressStart(WakeUp);
+    
+    button.attachClick(Co2Mode);
+
+    button.attachDoubleClick(Co2Mode);
 }
