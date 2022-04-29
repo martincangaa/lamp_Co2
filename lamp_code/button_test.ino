996420f52327b4c352f674e3f3e2231eec046c8b
martincangaa <91851271+martincangaa@users.noreply.github.com>
2022-04-30 00:13:04 +0200

    Update button_test.ino

--- lamp_code/button_test.ino
+++ lamp_code/button_test.ino
@@ -61,19 +61,80 @@ void loop(){
 }
 
 
-// 1.2 Version (mapping lamp modes) (I'm using OneButton library: https://github.com/mathertel/OneButton)
+// 1.2 Version (mapping lamp modes) (I'm using OneButton library: https://github.com/mathertel/OneButton) (I modified the library example "BlinkMachine" created by Matthias Hertel)
 
 #include "OneButton.h"
-#define BUTTON 2
-int button_touches = 0;
 
-OneButton button(BUTTON, false);
+typedef enum {
+  LAMP_OFF,  // set lamp "OFF".
+  LAMP_ON,   // set lamp "ON"
+  MODE_CO2  // blink LED "FAST"
+} 
+MyActions;
 
-void setup(){
-    
-    button.attachLongPressStart(WakeUp);
-    
-    button.attachClick(Co2Mode);
+#define PIN_INPUT 2
+#define PIN_LED 13
 
-    button.attachDoubleClick(Co2Mode);
-}
+
+OneButton button(PIN_INPUT, true);
+
+MyActions nextAction = LAMP_OFF; // lamp off when starting
+
+
+// setup code here, to run once.
+void setup() {
+  // enable the standard led on pin 13.
+  pinMode(PIN_LED, OUTPUT);      // sets the digital pin as output
+
+  // link the Press function to be called on a click event.   
+  button.attachLongPressStart(PressFunction);
+
+  // link the Click function to be called on a doubleclick event.   
+  button.attachClick(ClickFunction);
+
+  // set 80 msec. debouncing time. Default is 50 msec.
+  button.setDebounceTicks(80);
+} // setup
+
+
+void loop() {
+  unsigned long now = millis();
+
+  // keep watching the push button:
+  button.tick(); 
+
+  if (nextAction == LAMP_OFF) {
+    // turn lamp off
+    GoBed();
+
+  } else if (nextAction == LAMP_ON) {
+    // turn lamp on
+    WakeUp();
+
+  } else if (nextAction == MODE_CO2) {
+    // start mode CO2
+    CO2Mode();
+
+  } 
+} 
+
+
+// this function will be called when the button was held for 1 second.
+void PressFunction() {
+  if (nextAction == LAMP_OFF)
+    nextAction = LAMP_ON;
+  else
+    nextAction = LAMP_OFF;
+} // PressFunction
+
+
+// this function will be called when the button was pressed 1 time.
+void ClickFunction() {
+  if (nextAction == LAMP_ON) {
+    nextAction = MODE_CO2;
+
+  } else if (nextAction == MODE_CO2) {
+    nextAction = LAMP_ON;
+
+  } 
+} // ClickFunction
